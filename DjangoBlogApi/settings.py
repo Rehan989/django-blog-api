a6905a11839e095bae31980e5c46c27059945afe
Rehan989 <mdrehanmakrani@gmail.com>
2021-10-15 18:26:33 +0530

    Adding blog post and blog comments model created

--- DjangoBlogApi/settings.py
+++ DjangoBlogApi/settings.py
@@ -130,6 +130,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 
 STATIC_URL = '/static/'
 
@@ -163,3 +167,6 @@ DJOSER = {
         "user_delete": "djoser.serializers.UserSerializer",
     },
 }
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- blogapi/models.py
+++ blogapi/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.related import ForeignKey
+from django.utils.timezone import now
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
 
-# Create your models here.
+# Image compression
+def compress(image):
+    im = Image.open(image)
+    im = im.convert('RGB')
+    # create a BytesIO object
+    im_io = BytesIO() 
+    # save image to BytesIO object
+    im.save(im_io, 'JPEG', quality=25,optimize=True) 
+    # create a django-friendly Files object
+    new_image = File(im_io, name=image.name)
+    return new_image
+
+class Post(models.Model):
+    sno = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=55)
+    slug = models.CharField(max_length=130)
+    views = models.IntegerField(default=0)
+    content = models.TextField()
+    timestamp = models.DateTimeField(blank=True)
+    meta_description = models.CharField(max_length=500)
+    publish_state = models.CharField(max_length=10, choices=(('publish', 'PUBLISH'), ('draft', 'DRAFT')))
+    image = models.ImageField(upload_to='blog/image/', default="")
+
+    def save(self, *args, **kwargs):
+        if self.image:
+            new_img = compress(self.image)
+            self.image = new_img
+            super().save(*args, **kwargs)
+    def __str__(self):
+        return self.title
+
+
+class BlogComment(models.Model):
+    sno = models.AutoField(primary_key=True)
+    comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    timestamp = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.comment[0:13] + '...' + " by " + self.user.username
